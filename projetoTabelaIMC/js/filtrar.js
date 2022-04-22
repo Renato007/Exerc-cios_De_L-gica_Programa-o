e2cd1da1bdc4db5fc8e8877dfd588303517bb259
Renato Soares <renatosilsoa@gmail.com>
2022-04-22 10:45:50 -0300

    implementar a lógica de filtragem

--- projetoTabelaIMC/js/filtrar.js
+++ projetoTabelaIMC/js/filtrar.js
@@ -6,12 +6,26 @@ esconder quem não é igual ao texto de entrada
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function () {
-    //console.log(this.value); mostrar a capituração dos caracteres digitados no campo texto
+  //console.log(this.value); mostrar a capituração dos caracteres digitados no campo texto
   var pacientes = document.querySelectorAll(".paciente");
-  for (var i = 0; i < pacientes.length; i++) {
+
+  if (this.value.length > 0) {
+    console.log("tem algo digitado");
+    for (var i = 0; i < pacientes.length; i++) {
       var paciente = pacientes[i];
-      var tdNome = paciente.querySelector(".info-nome")
+      var tdNome = paciente.querySelector(".info-nome");
       var nome = tdNome.textContent;
-      console.log(nome); //mostra a lista dos nomes no for
+      //console.log(nome); //mostra a lista dos nomes no for
+      if (nome != this.value) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  }else{
+    for(var  i = 0; i < pacientes.length; i++){
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisivel")
+    }
   }
 });
