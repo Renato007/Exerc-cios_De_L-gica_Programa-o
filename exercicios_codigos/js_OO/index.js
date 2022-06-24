8dbfaee5e83b9ee4babf329c7e698a062f871928
Renato Soares <renatosilsoa@gmail.com>
2022-06-24 20:05:40 -0300

    aula 3 - valor privado, função return

--- exercicios_codigos/js_OO/index.js
+++ exercicios_codigos/js_OO/index.js
@@ -1,38 +1,39 @@
-class Cliente{
-    nome;
-    cpf;
+class Cliente {
+  nome;
+  cpf;
 }
-class ContaCorrente{
-    agencia;
-    saldo;
+class ContaCorrente {
+  agencia;
+  //#saldo = 0;
+  _saldo = 0;
 
-    sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor; //this significa deste(a)
-            console.log("valor retirado ", valor);
-        }else{
-            console.log("Saldo ", this.saldo);
-            console.log("Saldo insuficiente para o saque");
-        }
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor; //this significa deste(a)
+      return valor;
     }
+  }
+  depositar(valor) {
+    if (valor <= 0) return; // ele sai para isso comece com o que você não quer que aconteça.
+    this._saldo += valor;
+  }
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = "Renato";
 cliente1.cpf = 11122233304;
 
-
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88855577703;
 
 const contaCorrenteRenato = new ContaCorrente();
-contaCorrenteRenato.saldo = 0;
+contaCorrenteRenato._saldo = 0;
 contaCorrenteRenato.agencia = 1001;
-console.log(contaCorrenteRenato.saldo)
-contaCorrenteRenato.saldo = 400;
-console.log(contaCorrenteRenato.saldo);
-contaCorrenteRenato.sacar(400);
 
+contaCorrenteRenato.depositar(100);
 
-console.log("Saldo atual", contaCorrenteRenato.saldo);
+const valorSacado = contaCorrenteRenato.sacar(50);
+
+console.log(valorSacado);
+console.log(contaCorrenteRenato);
