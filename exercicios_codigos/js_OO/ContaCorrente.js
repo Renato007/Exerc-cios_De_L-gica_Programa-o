d4f4ac5956e619f0e98ea1a4a2e3b82c6c25c07b
Renato Soares <renatosilsoa@gmail.com>
2022-06-28 22:09:11 -0300

    aula 10 - relebrando o projeto e cliando class conta poupança

--- exercicios_codigos/js_OO/ContaCorrente.js
+++ exercicios_codigos/js_OO/ContaCorrente.js
@@ -2,10 +2,6 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
   static numeroDeContas = 0;
-  agencia;
-  _cliente;
-  //#saldo = 0;
-  _saldo = 0;
 
   set cliente(novoValor) {
     if (novoValor instanceof Cliente){
@@ -25,6 +21,7 @@ export class ContaCorrente {
   constructor(agencia, cliente){
     this.agencia = agencia;
     this.cliente = cliente;
+    this._saldo = 0;
     ContaCorrente.numeroDeContas += 1;
 
   }
--- exercicios_codigos/js_OO/index.js
+++ exercicios_codigos/js_OO/index.js
@@ -1,20 +1,15 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Renato", 11122233304);
-const cliente2 = new Cliente("Daiane", 88855577703);
-
 
 const contaCorrenteRenato = new ContaCorrente(1001, cliente1);
 contaCorrenteRenato.depositar(500);
+contaCorrenteRenato.sacar(100)
 
-const conta2 = new ContaCorrente(102, cliente2);
-
-let valor = 200;
-
-contaCorrenteRenato.transferir(valor, conta2);
-
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+console.log(contaPoupanca);
 console.log(contaCorrenteRenato);
-console.log(conta2);
-console.log(ContaCorrente.numeroDeContas)
+
 
