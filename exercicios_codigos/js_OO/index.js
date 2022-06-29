9d06a729c670106bb9161c86323ca6bb9c31c48b
Renato Soares <renatosilsoa@gmail.com>
2022-06-28 21:28:48 -0300

    aula 08 - construtores

--- exercicios_codigos/js_OO/index.js
+++ exercicios_codigos/js_OO/index.js
@@ -1,29 +1,18 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Renato";
-cliente1.cpf = 11122233304;
+const cliente1 = new Cliente("Renato", 11122233304);
+const cliente2 = new Cliente("Daiane", 88855577703);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Daiane";
-cliente2.cpf = 88855577703;
-
-const contaCorrenteRenato = new ContaCorrente();
-contaCorrenteRenato.agencia = 1001;
-contaCorrenteRenato.cliente = cliente1;
+const contaCorrenteRenato = new ContaCorrente(1001, cliente1);
 contaCorrenteRenato.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2; // verifica se é do tipo cliente caso não é  aparecera undefined
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
 
-let valor = 100;
+let valor = 200;
 
 contaCorrenteRenato.transferir(valor, conta2);
 
-//conta2.saldo = 400; //provoca erro não deixa alterar apenas pegar
 console.log(contaCorrenteRenato);
 console.log(conta2);
-console.log(conta2.saldo);
-console.log(contaCorrenteRenato.saldo);
+
