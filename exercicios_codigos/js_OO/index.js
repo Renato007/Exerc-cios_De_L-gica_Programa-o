7a37eff3645aca5005d16391d79f9d9768cb116a
Renato Soares <renatosilsoa@gmail.com>
2022-06-30 16:30:19 -0300

    aula 12

--- exercicios_codigos/js_OO/index.js
+++ exercicios_codigos/js_OO/index.js
@@ -1,15 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Renato", 11122233304);
 
-const contaCorrenteRenato = new Conta(0, cliente1, 1001);
+const contaCorrenteRenato = new ContaCorrente( cliente1, 1001);
 contaCorrenteRenato.depositar(500);
 contaCorrenteRenato.sacar(100)
 
-const contaPoupanca = new Conta(50, cliente1, 1001)
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 console.log(contaPoupanca);
 console.log(contaCorrenteRenato);
 
