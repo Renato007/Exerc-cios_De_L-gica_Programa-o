f89390251a81799d4b8c184ab70ecb5977a1252c
Renato Soares <renatosilsoa@gmail.com>
2022-07-08 21:08:02 -0300

    aula19 - introdução a interface

--- exercicios_codigos/js_OO/funcionario/Funcionario.js
+++ exercicios_codigos/js_OO/funcionario/Funcionario.js
@@ -1,16 +1,16 @@
-export class Funcionario{
-        constructor(nome, salario, cpf){
-            this._nome = nome;
-            this._salario = salario;
-            this._cpf = cpf;
+export class Funcionario {
+  constructor(nome, salario, cpf) {
+    this._nome = nome;
+    this._salario = salario;
+    this._cpf = cpf;
 
-            this._bonificacao = 1;
-            this._senha;
-        }
-        get senha(){
-            return this._senha
-        }      
-        cadastrarSenha(senha){
-            this._senha = senha;
-        }  
-    }
+    this._bonificacao = 1;
+    this._senha;
+  }
+  autenticar(senha) {
+    return senha == this._senha;
+  }
+  cadastrarSenha(senha) {
+    this._senha = senha;
+  }
+}
--- exercicios_codigos/js_OO/index.js
+++ exercicios_codigos/js_OO/index.js
@@ -1,11 +1,18 @@
 import { Gerente } from "./funcionario/gerente.js";
 import { Diretor } from "./funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+import { Cliente } from "./Cliente.js";
 
 const diretor = new Diretor("Renato", 10000, 12345678900);
 diretor.cadastrarSenha("123456");
 const gerente = new Gerente ("Gustavo", 5000, 11122233312);
 gerente.cadastrarSenha("11111");
 
-const estaLogado = SistemaAutenticacao.login(gerente,"11111");
-console.log(estaLogado)
+const cliente = new Cliente("Duane", 76543218900, "456")
+
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente,"11111");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor,"123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente,"456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado)
+
